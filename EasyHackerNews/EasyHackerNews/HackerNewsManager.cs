cc596a880b0dcc4ce452f5a7d954086a73813ada
Amit Mittal <amit.mittal1993@gmail.com>
2018-01-27 20:29:57 +0530

    Basic Functionality Added
    - Fetching top news from HackerNews working
    - Clicking the item in list redirects to the URL

---
+++ EasyHackerNews/EasyHackerNews/HackerNewsManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EasyHackerNews
+{
+    internal class HackerNewsManager
+    {
+        private static string ApiUrl = "https://node-hnapi.herokuapp.com/news";
+
+        public List<HackerNews> DoWork()
+        {
+            string html = string.Empty;
+            string url = ApiUrl;
+            List<HackerNews> newsList = new List<HackerNews>();
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(html);
+
+            var items = JsonConvert.DeserializeObject<JArray>(html);
+            foreach (var item in items)
+            {
+                HackerNews news = new HackerNews();
+                news.title = (string) item["title"];
+                news.url = (string) item["url"];
+                news.points = 0; //(item["points"] == null ? 0 : (int) item["points"])
+
+                newsList.Add(news);
+            }
+
+            return newsList;
+        }
+    }
+
+    /// <summary>
+    /// "id":16245142,
+    /// "title":"An Italian Song That Sounds Like English But Is Nonsense",
+    /// "points":213,
+    /// "user":"idiocratic",
+    /// "time":1517035111,
+    /// "time_ago":"7 hours ago",
+    /// "comments_count":99,
+    /// "type":"link",
+    /// "url":"https://www.atlasobscura.com/articles/deep-roots-italian-song-sounds-like-english-american-medieval-comedy-nonsense",
+    /// "domain":"atlasobscura.com"
+    /// </summary>
+    public class HackerNews
+    {
+        public int id;
+
+        public string title;
+
+        public int points;
+
+        public string user;
+
+        public long time;
+
+        public string time_ago;
+
+        public int comments_count;
+
+        public string type;
+
+        public string url;
+
+        public string domain;
+    }
+}
---
+++ EasyHackerNews/EasyHackerNews/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyHackerNews
+{
+    static class Program
+    {
+        private static Form1 f;
+        private static List<HackerNews> list;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            f = new Form1();
+            HackerNewsManager m = new HackerNewsManager();
+            list = m.DoWork();
+
+            foreach (HackerNews news in list)
+            {
+                ListViewItem listItem = new ListViewItem(news.title);
+                f.listView1.Items.Add(listItem);
+            }
+
+            f.listView1.SelectedIndexChanged += _listBox1_SelectedIndexChanged;
+
+            Application.Run(f);
+        }
+
+        private static void _listBox1_SelectedIndexChanged(object pSender, EventArgs pArgs)
+        {
+            ListView.SelectedListViewItemCollection breakfast =
+                f.listView1.SelectedItems;
+            foreach (ListViewItem item in breakfast)
+            {
+                Process.Start(list[item.Index].url);
+            }
+        }
+    }
+}
